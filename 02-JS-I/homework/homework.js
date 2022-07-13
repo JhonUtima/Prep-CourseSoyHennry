a579fc6c707f3b4278b9a71a575c3872b74cf3a2
unknown <jhoncorreaa@gmail.com>
2022-07-13 01:10:30 -0500

    haciendo la tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "prueba";
 const nuevoNum = 18;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
